e4d4ac982cb5ddd5963297815795f9c4d7e0dc49
perako2023 <135764815+perako2023@users.noreply.github.com>
2023-08-19 12:53:58 -0500

    feat: simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
 import './App.css'
 
 export const App = () => {
-  return <></>
+  return <Layout />
+}
+
+const Layout = () => {
+  return (
+    <div className="root-grid">
+      <header className="root__header">
+        <button>🍔</button>
+        <h1>Audzone</h1>
+      </header>
+      <aside className="root__aside">
+        <nav className="navbar">
+          <a className="nav-link">Browse</a>
+          <a className="nav-link">Settings</a>
+          <a className="nav-link">Downloads</a>
+          <a className="nav-link">Library</a>
+        </nav>
+      </aside>
+      <main className="root__main">
+        <section className="media-player">
+          <img
+            src="https://miro.medium.com/v2/resize:fit:1400/1*sVSPf1ZdHnSSmAfDm328Hg.png"
+            alt="thumbnail"
+          />
+        </section>
+        <section className="playlist">
+          <ul>
+            <li>playable item</li>
+            <li>playable item</li>
+            <li>playable item</li>
+            <li>playable item</li>
+          </ul>
+        </section>
+      </main>
+
+      <footer className="root__footer">Footer</footer>
+    </div>
+  )
 }
